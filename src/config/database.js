32922723998865b8d66641fb5b282fba50b02b57
Manh Pham <manhpham90vn@icloud.com>
2024-01-16 04:42:25 +0000

    Update .gitignore and database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,17 @@
 import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
 
-const prisma = new PrismaClient()
-
-// const prisma = new PrismaClient().$extends({
-// 	query: {
-// 		user: {
-// 			$allOperations({ operation, args, query }) {
-// 				if (['create', 'update'].includes(operation) && args.data['password']) {
-// 					args.data['password'] = bcrypt.hashSync(args.data['password'], 10)
-// 				}
-// 				return query(args)
-// 			}
-// 		}
-// 	}
-// })
+const prisma = new PrismaClient().$extends({
+	query: {
+		user: {
+			$allOperations({ operation, args, query }) {
+				if (['create', 'update'].includes(operation) && args.data['password']) {
+					args.data['password'] = bcrypt.hashSync(args.data['password'], 10)
+				}
+				return query(args)
+			}
+		}
+	}
+})
 
 export default prisma
